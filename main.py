d0958c2867d7ecdd64e6529cae4da76d54115f51
Grzegorz Czaplik <faflik@onet.pl>
2021-04-10 20:48:08 +0200

    path to file

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ from datetime import datetime
 json_data = dict()
 current_ip = ""
 
+'''
+    path to file e.g. /home/<user>/PublicIPChangeLogToJson/
+'''
+path_to_file = ""
+
+file_to_write = path_to_file+"collections_ip.json"
 
 def write_json():
-    with open("collections_ip.json", "w") as f:
+    with open(file_to_write, "w") as f:
         json.dump(json_data, f, indent=4)
 
 
-def read_json(filename="collections_ip.json"):
-    with open(filename) as j_f:
+def read_json():
+    with open(file_to_write) as j_f:
         global json_data
         json_data = json.load(j_f)
 
